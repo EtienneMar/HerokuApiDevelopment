9bf50a5516672b1d67fee5559fea04edcc5796b8
EtienneMar <etienne.martinc@gmail.com>
2024-07-24 21:52:16 +0200

    Moving procFile and app logging

--- app.py
+++ app.py
@@ -70,49 +70,51 @@ def predict():
         sk_id_curr = request.args.get('SK_ID_CURR')
 
         if not sk_id_curr:
-            logger.warning("SK_ID_CURR non fourni dans la requête")
+            app.logger.warning("SK_ID_CURR non fourni dans la requête")
             return jsonify({'error': 'Veuillez fournir SK_ID_CURR en paramètre.'}), 400
 
-        logger.info(f"SK_ID_CURR reçu : {sk_id_curr}")
+        app.logger.info("SK_ID_CURR reçu %s", {sk_id_curr})
 
         # Vérifier que SK_ID_CURR peut être converti en entier
         try:
             sk_id_curr = int(sk_id_curr)
+            app.logger.info("Type de sk_id_curr = %s", {type(sk_id_curr)})
         except ValueError:
-            logger.error(f"SK_ID_CURR {sk_id_curr} ne peut pas être converti en entier.")
+            app.logger.error("SK_ID_CURR %s ne peut pas être converti en entier.", sk_id_curr)
             return jsonify({'error': f'SK_ID_CURR {sk_id_curr} ne peut pas être converti en entier.'}), 400
+        
+        app.logger.info("Valeurs de df_prediction['SK_ID_CURR'] (premiers 10) : %s", df_prediction['SK_ID_CURR'].tolist()[:10])
+        #logger.info(f"data_row is : {df_prediction['SK_ID_CURR'] == sk_id_curr}")
         
         try:
             # Récupérer les données correspondant à SK_ID_CURR depuis df_prediction
             
-            logger.info(f"Valeurs de df_prediction['SK_ID_CURR'] : {df_prediction['SK_ID_CURR'].tolist()}")
-            logger.info(f"Type de sk_id_curr : {type(sk_id_curr)}")
-            logger.info(f"data_row is : {df_prediction['SK_ID_CURR'] == sk_id_curr}")
+     
             data_row = df_prediction[df_prediction['SK_ID_CURR'] == sk_id_curr]
 
         except KeyError:
-            logger.error(f"SK_ID_CURR n'existe pas dans les colonnes de df_prediction")
-            return jsonify({'error': f"SK_ID_CURR n'existe pas dans les colonnes de df_prediction"}), 400
+            app.logger.error("SK_ID_CURR n'existe pas dans les colonnes de df_prediction")
+            return jsonify({'error'"SK_ID_CURR n'existe pas dans les colonnes de df_prediction"}), 400
         except Exception as e:
-            logger.error(f"Erreur dans la récupération des données: {e}")
+            app.logger.error("Erreur dans la récupération des données : %s", e)
             return jsonify({'error': str(e)}), 400
 
         if data_row.empty:
-            logger.warning(f"Aucune donnée trouvée pour SK_ID_CURR {sk_id_curr}")
+            app.logger.warning("Aucune donnée trouvée pour SK_ID_CURR %s", sk_id_curr)
             return jsonify({'error': f'Aucune donnée trouvée pour SK_ID_CURR {sk_id_curr}.'}), 404
 
         try:
             df = data_row.copy()
             # Vérifiez la forme des données avant la prédiction
-            logger.info(f"Shape des données avant prédiction : {df.shape}")
+            app.logger.info("Shape des données avant prédiction : %s", df.shape)
 
             # Réorganiser les colonnes selon l'ordre attendu par le modèle
             df = df[cols_when_model_builds]
         except KeyError as e:
-            logger.error(f"Erreur lors de la réorganisation des colonnes: {e}")
+            app.logger.error("Erreur lors de la réorganisation des colonnes: %s", e)
             return jsonify({'error': f"Les colonnes nécessaires pour le modèle sont manquantes : {str(e)}"}), 400
         except Exception as e:
-            logger.error(f"Erreur lors de la préparation des données pour la prédiction: {e}")
+            app.logger.error("Erreur lors de la préparation des données pour la prédiction: %s", e)
             return jsonify({'error': str(e)}), 400
 
         try:
@@ -129,12 +131,15 @@ def predict():
             return jsonify(result), 200
 
         except Exception as e:
-            logger.error(f"Erreur lors de la prédiction : {e}")
+            app.logger.error(f"Erreur lors de la prédiction : {e}")
             return jsonify({'error': str(e)}), 400
 
     except Exception as e:
-        logger.error(f"Erreur lors de la gestion de la requête : {e}")
+        app.logger.error(f"Erreur lors de la gestion de la requête : {e}")
         return jsonify({'error': str(e)}), 400
 
 if __name__ == '__main__':
     app.run(debug=True)
+    gunicorn_logger = logging.getLogger('gunicorn.error')
+    app.logger.handlers = gunicorn_logger.handlers
+    app.logger.setLevel(gunicorn_logger.level)
